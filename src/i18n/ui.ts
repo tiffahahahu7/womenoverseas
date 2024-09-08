a7ef7a9349c237e2c6339c4e3017b425de11521f
Mengqi Hu <hu000106@algonquinlive.com>
2024-09-08 17:33:06 -0400

    Initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  zh: "ZH",
+  en: "EN",
+};
+
+export const defaultLang = "zh";
+
+export const ui = {
+  zh: {
+    "nav.home": "主页",
+    "nav.events": "活动",
+    "nav.q-a": "Q&A",
+    "nav.about": "关于她乡",
+    "nav.support": "支持她乡",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.events": "Events",
+    "nav.q-a": "Q&A",
+    "nav.about": "About",
+    "nav.support": "Support",
+  },
+} as const;
+
+export const showDefaultLang = false;
