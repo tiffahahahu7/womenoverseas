b50d332589d9a1798f474c4c2dc630ca27efae67
Mengqi Hu <hu000106@algonquinlive.com>
2024-09-08 21:16:40 -0400

    fix type error for lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "zh",
-        locales: ["zh", "en"],
-      }
+  i18n: {
+      defaultLocale: "zh",
+      locales: ["zh", "en"],
+    },
+
+  output: "server",
+  adapter: netlify()
 });
